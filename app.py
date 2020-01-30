cdd01f724fe3079b020cd8b255955aa80fa78ba2
Naitik Rathod <rathodnaitik10@gmail.com>
2020-01-30 11:30:53 +0530

    added files

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask,render_template,url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,render_template,url_for,flash,session,logging,request
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+#__file__ ---> C://.../SIH2020/town_flask/app.py
+
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(80))
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route("/login",methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        uname = request.form["uname"]
+        passw = request.form["passw"]
+        
+        login = user.query.filter_by(username=uname, password=passw).first()
+        if login is not None:
+            return redirect(url_for("index"))
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        uname = request.form['uname']
+        mail = request.form['mail']
+        passw = request.form['passw']
+
+        register = user(username = uname, email = mail, password = passw)
+        db.session.add(register)
+        db.session.commit()
+
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
